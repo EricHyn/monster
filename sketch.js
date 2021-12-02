a5fe765fb8dd6c8b192174d244936cd6abb7c0c4
EricHyn <95388726+EricHyn@users.noreply.github.com>
2021-12-01 20:13:32 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+let player = [];
+let xPos;
+let dir;
+
+function preload() {
+  player[0] = loadImage('data/leftmonster.png');
+  player[1] = loadImage('data/rightmonster.png');
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+} 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  xPos = 0;
+  dir = 0;
+}
+
+function draw() {
+  background(255);
+  image(player[dir], xPos, height-500, 500, 500);
+}
+
+function mouseMoved(){
+  xPos = mouseX;
+  if (mouseX - pmouseX < 0){
+    dir = 0;
+  }
+  else{
+    dir = 1;
+  }
+}
+
+function touchMoved(){
+  xPos = mouseX;
+  if (mouseX - pmouseX < 0){
+    dir = 0;
+  }
+  else{
+    dir = 1;
+  }
+}
